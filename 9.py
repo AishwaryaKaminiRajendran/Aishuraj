de062764943f67efef30697e3077f5671c1b041a
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-01-26 20:15:52 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,12 @@
+a,b=map(int,input().split())
+x=' '
+for num in range(a+b,b):
+	if num>1:
+		for i in range(2,num):
+			if num%i==0:
+				break
+			else:
+				x=x+str(num)+' '
+print(x.strip())
+		
+		
