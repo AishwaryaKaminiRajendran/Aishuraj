395f8a492ae322e7b1df2509b7de9d0fbaa817d3
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-01-26 20:12:11 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,22 @@
+# your code goes here
+a,b=input().split()
+c=" "
+d=" "
+g=" "
+for i in range(0,len(a)):
+	if i==0:
+		c=a[i].upper()
+	else:
+		c=a[i].lower()
+	#e=print(str(c),end="")
+	#print(str(c),end="")
+for i in range(0,len(a)):
+	if i==0:
+		d=b[i].upper()
+	else:
+		d=b[i].lower()
+	#f=print(str(d),end="")
+
+#print(str(c),end=""+str(d),end="")
+	#print(str(d),end="")
+#print(str(c),end=" ",str(d))
