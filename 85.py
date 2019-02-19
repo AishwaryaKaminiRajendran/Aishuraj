01b57d5c9bf7fd4ffa9868fb722b2916fcc7fc85
AishwaryaKaminiRajendran <46441583+AishwaryaKaminiRajendran@users.noreply.github.com>
2019-02-19 11:35:08 +0530

    Update 85.py

--- 85.py
+++ 85.py
@@ -1,2 +1,11 @@
-n=int(input())
-print(~n)
+n=input()
+x=' '
+y=' '
+for i in range(0,len(n)):
+	if i%2==0:
+		x=x+n[i]
+print(x.strip())
+for j in range(0,len(n)):
+	if j%2==1:
+		y=y+n[j]
+print(y.strip())
